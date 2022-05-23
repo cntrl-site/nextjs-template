f1a7d821a51c546207046ca039d861c314fc88b2
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-05-23 19:41:32 +0300

    foundation for the public site

---
+++ cntrl-client/CntrlClient.ts
@@ -0,0 +1,40 @@
+import { Article, Project } from './Format';
+
+const API_URL = process.env.CNTRL_API_URL;
+
+export class CntrlClient {
+  constructor(
+    private projectId: string
+  ) {}
+
+  async getProject(): Promise<Project> {
+    const response = await fetch(`${API_URL}/projects/${this.projectId}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch project with id #${this.projectId}: ${response.statusText}`);
+    }
+    // TODO fix this stupid typing
+    const data = await response.json() as Project;
+    return data;
+  }
+
+  async getPageArticle(pageSlug: string): Promise<Article> {
+    const projectResponse = await fetch(`${API_URL}/projects/${this.projectId}`);
+    if (!projectResponse.ok) {
+      throw new Error(`Failed to fetch project with id #${this.projectId}: ${projectResponse.statusText}`);
+    }
+    const data = await projectResponse.json() as Project;
+    const page = data.pages.find((page) => page.slug === pageSlug);
+    if (!page) {
+      throw new Error(`Page with a slug ${pageSlug} was noy found in project with id #${this.projectId}`);
+    }
+
+    const articleResponse = await fetch(`${API_URL}/articles/${page.articleId}`);
+    if (!articleResponse.ok) {
+      throw new Error(`Failed to fetch article with id #${page.articleId}: ${articleResponse.statusText}`);
+    }
+    // TODO fix this stupid typing
+    const articleData = await articleResponse.json() as Article;
+
+    return articleData;
+  }
+}
