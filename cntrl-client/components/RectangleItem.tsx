511aeb70cdc06eb34ed8ab9661de141b6a51556e
sergokiko <sergokiko@gmail.com>
2022-06-27 14:52:07 +0300

    feat(rich text item): fix indent

--- cntrl-client/components/RectangleItem.tsx
+++ cntrl-client/components/RectangleItem.tsx
@@ -9,21 +9,19 @@ const RectangleItem: FC<ItemProps<RectangleItem>> = ({ item, layouts }) => (
     <>
       <div className={`rectangle-${item.id}`} />
       <style jsx>{`
-      ${
-        getLayoutStyles(layouts, [item.layoutParams], ([{ strokeColor, fillColor, radius, strokeWidth }]) => (`
-           .rectangle-${item.id} {
-              position: absolute;
-              width: 100%;
-              height: 100%;
-              border-style: solid;
-              box-sizing: border-box;
-              border-color: ${strokeColor};
-              background-color: ${fillColor};
-              border-radius: ${radius * 100}vw;
-              border-width: ${strokeWidth * 100}vw;
-            }`
-        ))
-      }
+      ${getLayoutStyles(layouts, [item.layoutParams], ([{ strokeColor, fillColor, radius, strokeWidth }]) => (`
+         .rectangle-${item.id} {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border-style: solid;
+            box-sizing: border-box;
+            border-color: ${strokeColor};
+            background-color: ${fillColor};
+            border-radius: ${radius * 100}vw;
+            border-width: ${strokeWidth * 100}vw;
+          }`
+        ))}
       `}</style>
     </>
   </LinkWrapper>
