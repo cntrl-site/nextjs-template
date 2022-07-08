ee86f88ef8144cc8f032f3d6785f37631d113637
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-07-08 12:36:31 +0300

    [CNTRL-114] fir for rich text keys

--- cntrl-client/components/RichTextItem.tsx
+++ cntrl-client/components/RichTextItem.tsx
@@ -8,27 +8,31 @@ const richTextConv = new RichTextConv();
 
 const RichTextItem: FC<ItemProps<RichTextItem>> = ({ item, layouts }) => {
   const sortedLayouts = layouts.slice().sort((a, b) => a.startsWith - b.startsWith);
+  const styles = sortedLayouts.map((l, i) => {
+    console.log(l);
+    const next = sortedLayouts[i + 1];
+    return (
+      `
+        .rich-text-${l.id} {
+          display: none;
+        }
+        @media (min-width: ${l.startsWith}px) and (max-width: ${next ? next.startsWith : Number.MAX_SAFE_INTEGER}px ) {
+          .rich-text-${l.id} {
+            display: block;
+          }
+        }
+      `
+    );
+  }).join('\n');
 
   return (
     <>
-      {
-        sortedLayouts.map((l, i) => {
-          const next = sortedLayouts[i + 1];
-          return <>
-            <div key={l.id} className={`rich-text-${l.id}`}>{richTextConv.toHtml(item, l.id, layouts)}</div>
-            <style jsx>{`
-              .rich-text-${l.id} {
-                display: none;
-              }
-              @media (min-width: ${l.startsWith}px) and (max-width: ${next ? next.startsWith : Number.MAX_SAFE_INTEGER}px ) {
-                .rich-text-${l.id} {
-                  display: block;
-                }
-              }
-            `}</style>
-          </>
-        })
-      }
+      {sortedLayouts.map((l) => (
+        <div key={l.id} className={`rich-text-${l.id}`}>{richTextConv.toHtml(item, l.id, layouts)}</div>
+      ))}
+      <style jsx>
+        {styles}
+      </style>
     </>
   );
 };
