3c562116c8652c8891c011c90eb13c65c41074b9
Taras Kustov <tkusto@gmail.com>
2022-11-13 17:11:55 +0200

    feat: custom items support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/reset.css';
 import type { AppProps } from 'next/app';
+import { CntrlProvider } from '@cntrl-site/sdk-nextjs';
+
+// place for your custom items definitions
+// CustomItems.define('customItem1', CustomItem1);
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CntrlProvider>
+      <Component {...pageProps} />
+    </CntrlProvider>
+  );
 }
 
 export default App;
