aa69efaff29ca937daf3b75d0986afea1327e8f0
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2024-05-01 18:13:54 +0200

    SDK 0.x.x -> 1.x.x

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  trailingSlash: true
+  trailingSlash: true,
+  output: 'export',
+  distDir: '_static'
 };
 
 module.exports = nextConfig;
