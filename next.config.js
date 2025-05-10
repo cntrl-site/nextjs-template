17b51e66f98615bc82d126a3080b8222adf1ce4f
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2025-05-10 12:15:42 +0200

    base path for the multisite load balancer for staging

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
   reactStrictMode: false,
-  trailingSlash: true
+  trailingSlash: true,
+  basePath
 };
 
 module.exports = nextConfig;
