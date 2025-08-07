c60a9d781a28196f44a46cb3e2575ec0d85c3695
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2025-08-07 17:22:36 +0200

    bump sdk

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
-  basePath
 };
 
 module.exports = nextConfig;
