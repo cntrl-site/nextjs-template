491623b4ae4ee85c0f775f386e03c41e103ad007
sergokiko <sergokiko@gmail.com>
2025-09-02 14:41:41 +0200

    code-export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
+const isCodeExport = process.env.CNTRL_BUILD_MODE === 'code-export';
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
   output: 'export',
-  distDir: '_static'
+  distDir: '_static',
+  assetPrefix: isCodeExport ? './' : undefined
 };
 
 module.exports = nextConfig;
