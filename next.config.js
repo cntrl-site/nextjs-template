1f13d973a62e0e826aa317678582a112be9f9895
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2025-09-23 16:28:14 +0200

    fix staging

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const isCodeExport = process.env.CNTRL_BUILD_MODE === 'self-hosted';
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
-  output: 'export',
-  distDir: '_static',
-  assetPrefix: isCodeExport ? './' : undefined,
+  basePath
 };
 
 module.exports = nextConfig;
