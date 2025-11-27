9830d386e900acfa83703bc343f5ebed80ff619e
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2025-11-27 13:58:18 +0100

    Revert "Cntrl 2408 staging"

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+const isCodeExport = process.env.CNTRL_BUILD_MODE === 'self-hosted';
+
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
-  basePath
+  output: 'export',
+  distDir: '_static',
+  assetPrefix: isCodeExport ? './' : undefined,
 };
 
 module.exports = nextConfig;
