aec95448704a8cbb71a7c270dea882765a583669
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2026-04-17 20:30:22 +0200

    components v1

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -17,13 +17,16 @@ export const getServerSideProps: GetServerSideProps<PageProps, ParamsWithSlug> =
   const client = new CntrlClient(process.env.CNTRL_API_URL!);
   const originalSlug = params?.slug;
   const slug = Array.isArray(originalSlug) ? originalSlug.join('/') : '';
+  const publicApiBase = client.getHostname();
+  cntrlSdkContext.setPublicApiBase(publicApiBase);
   const cntrlPageData = await client.getPageData(slug, buildMode === 'self-hosted' ? 'self-hosted' : 'default');
   const sectionData = await cntrlSdkContext.resolveSectionData(cntrlPageData.article.sections);
 
   return {
     props: {
       ...cntrlPageData,
-      sectionData
+      sectionData,
+      publicApiBase
     }
   };
 };
